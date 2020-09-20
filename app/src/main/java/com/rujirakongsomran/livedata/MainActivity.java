0121613a101a225939f522a64bfdf090e440e528
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-20 15:24:40 +0700

    Use view binding in activities

--- app/src/main/java/com/rujirakongsomran/livedata/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/livedata/MainActivity.java
@@ -3,12 +3,21 @@ package com.rujirakongsomran.livedata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rujirakongsomran.livedata.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
